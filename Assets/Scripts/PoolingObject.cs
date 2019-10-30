95058b4fca4b5bf5126b511037f451951aef6984
Vishnu <vishnumnn4@gmail.com>
2019-10-29 22:06:26 -0500

    Added equals override for PoolingObject class so that tryGetValue won't fail when looking up a pooling object in dictionaries.

--- Assets/Scripts/PoolingObject.cs
+++ Assets/Scripts/PoolingObject.cs
@@ -13,5 +13,15 @@ namespace Assets.Scripts
         public int poolSize;
         public GameObject objectToPool;
         public bool expandable;
+
+        public override bool Equals(object obj)
+        {
+            PoolingObject p = (obj as PoolingObject);
+            if (p.objectToPool.Equals(objectToPool) && p.poolSize == poolSize && p.expandable == expandable)
+            {
+                return true;
+            }
+            return false;
+        }
     }
 }
